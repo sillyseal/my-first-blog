7b73e456ca8065d2aaff1f4fcc97fff97f8be706
jiexiang <jxyong>
2018-07-29 23:53:48 +0800

    add blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ajafnhk@92s$1-d_ka&kkbgc1%@n+r$4yaehb3=^c5)yp_9s--'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']  # default is ['localhost', '127.0.0.1', '[::1]']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog'
 ]
 
 MIDDLEWARE = [
